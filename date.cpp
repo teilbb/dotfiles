3bf33fc6457bc92ed6e1078a612b15e9bf630f83
teilbb <tielbb@sina.com>
2018-02-11 16:28:50 +0800

    Delete date.cpp

--- date.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <iomanip>
-#include <ctime>
-int main()
-{
-auto t = std::time(nullptr);
-auto tm = *std::localtime(&t);
-std::cout << std::put_time(&tm, "%Y-%m-%d") << std::endl;
-}
